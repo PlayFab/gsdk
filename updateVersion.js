aa43865b1284dedf7f7474d9b387fcc0e9dec37e
haroniti <haronid.m@microsoft.com>
2018-10-24 14:25:10 -0700

    Adding ability to tag nuget packages with beta

--- updateVersion.js
+++ updateVersion.js
@@ -1,7 +1,7 @@
 var fileName = process.argv[2]; 
 console.log("Begin updating version");
 
-var versionRe = new RegExp("([0-9]+)\.([0-9]+)\.[0-9][0-9][0-9][0-9][0-9][0-9]");
+var versionRe = new RegExp("([0-9]+)\.([0-9]+)\.([0-9][0-9][0-9][0-9][0-9][0-9])([\w-]*)");
 var now = new Date();
 var todaysDate = (now.getUTCFullYear()%100).toString().padStart(2, "0") + (now.getMonth()+1).toString().padStart(2, "0") + (now.getUTCDate()).toString().padStart(2, "0");
 
@@ -18,7 +18,7 @@ fs.readFile(fileName, 'utf8', function (err,data) {
   var minorVer = parseInt(reMatch[2]);
   minorVer++; // Increment the minor version here
 
-  var newVersion = majorVer.toString() + "." + minorVer.toString() + "." + todaysDate;
+  var newVersion = majorVer.toString() + "." + minorVer.toString() + "." + todaysDate + reMatch[4];
 
   fs.writeFile (fileName, newVersion, function(err) {
                 if (err) throw err;
