1a36e1d196a07388a26282ca4cc8d7a7187c4faa
Rakesh Varna <36653801+ravarnamsft@users.noreply.github.com>
2019-06-20 12:35:41 -0700

    Add unity GSDK. (#29)

---
+++ UnityGsdk/Assets/PlayFabSdk/MultiplayerAgent/Model/ConnectedPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace PlayFab.MultiplayerAgent.Model
+{
+    using System;
+
+    [Serializable]
+    public class ConnectedPlayer
+    {
+        public ConnectedPlayer(string playerid)
+        {
+            PlayerId = playerid;
+        }
+
+        public string PlayerId { get; set; }
+    }
+}
---
+++ UnityGsdk/Assets/PlayFabSdk/MultiplayerAgent/Model/GameServerConnectionInfo.cs
@@ -0,0 +1,42 @@
+﻿namespace PlayFab.MultiplayerAgent.Model
+{
+    using System.Collections.Generic;
+
+    public class GameServerConnectionInfo
+    {
+        public GameServerConnectionInfo()
+        {
+        }
+
+        public string PublicIpV4Adress { get; set; }
+
+        public IEnumerable<GamePort> GamePortsConfiguration { get; set; }
+    }
+
+    /// <summary>
+    /// A class that captures details about a game server port.
+    /// </summary>
+    public class GamePort
+    {
+        public GamePort()
+        {
+        }
+
+        /// <summary>
+        /// The friendly name / identifier for the port, specified by the game developer in the Build configuration.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The port at which the game server should listen on (maps externally to <see cref="ClientConnectionPort" />).
+        /// For process based servers, this is determined by Control Plane, based on the ports available on the VM.
+        /// For containers, this is specified by the game developer in the Build configuration.
+        /// </summary>
+        public int ServerListeningPort { get; set; }
+
+        /// <summary>
+        /// The public port to which clients should connect (maps internally to <see cref="ServerListeningPort" />).
+        /// </summary>
+        public int ClientConnectionPort { get; set; }
+    }
+}
