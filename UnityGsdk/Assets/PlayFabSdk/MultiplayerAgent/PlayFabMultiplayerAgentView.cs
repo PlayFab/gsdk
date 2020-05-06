1e8e3581d8f3d7fce832fee54c889c554bc71cb9
Georg Laage <georg@laage.de>
2020-05-06 18:21:47 +0200

    added possible fix for issue #49
    Warning: has yet to be tested

--- UnityGsdk/Assets/PlayFabSdk/MultiplayerAgent/PlayFabMultiplayerAgentView.cs
+++ UnityGsdk/Assets/PlayFabSdk/MultiplayerAgent/PlayFabMultiplayerAgentView.cs
@@ -7,8 +7,34 @@ namespace PlayFab
 
     public class PlayFabMultiplayerAgentView : MonoBehaviour
     {
+		/// <summary>
+		/// Static reference to the current active instance of the PlayFabMultiplayerAgentView.
+		/// </summary>
+		public static PlayFabMultiplayerAgentView current { get; private set; };
+		
         private float _timer;
 
+		/// <summary>
+		/// Awake constructor
+		/// </summary>
+		private void Awake()
+		{
+			Debug.Log($"{Time.fixedTime} PlayFabMultiplayerAgentView awake ");
+			// Check if the static instance already contains a refernce:
+			if (current)
+			{
+				// Destroy this instance since we only ever need one PlayFabMultiplayerAgentView
+				Destroy(gameObject);
+				return;
+			}
+			else
+			{
+				// Need to keep this game object alive through scene changes.
+				DontDestroyOnLoad(this);
+				current = this;
+			}
+		}
+		
         private void LateUpdate()
         {
             if (PlayFabMultiplayerAgentAPI.CurrentState == null)
@@ -57,5 +83,13 @@ namespace PlayFab
                 StartCoroutine(PlayFabMultiplayerAgentAPI.SendHeartBeatRequest());
             }
         }
+		
+		/// <summary>
+		/// Called when gameobject is destroyed
+		/// </summary>
+		private void OnDestroy()
+		{
+			Debug.Log($"{Time.fixedTime} PlayFabMultiplayerAgentView destroyed ");
+		}
     }
 }
