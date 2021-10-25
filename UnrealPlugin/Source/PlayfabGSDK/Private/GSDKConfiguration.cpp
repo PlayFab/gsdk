1aef768bde54d3553adae62030d749079faac82d
Paul Gilmore <pagilmor@microsoft.com>
2021-10-25 11:03:25 -0700

    Add missing TEXT() wrappers (#94)

--- UnrealPlugin/Source/PlayfabGSDK/Private/GSDKConfiguration.cpp
+++ UnrealPlugin/Source/PlayfabGSDK/Private/GSDKConfiguration.cpp
@@ -150,75 +150,75 @@ FJsonFileConfiguration::FJsonFileConfiguration(const FString& FileName)
 		return;
 	}
 
-	HeartbeatEndpoint = ConfigJson->GetStringField("heartbeatEndpoint");
-	ServerId = ConfigJson->GetStringField("sessionHostId");
-	LogFolder = ConfigJson->GetStringField("logFolder");
-	SharedContentFolder = ConfigJson->GetStringField("sharedContentFolder");
+	HeartbeatEndpoint = ConfigJson->GetStringField(TEXT("heartbeatEndpoint"));
+	ServerId = ConfigJson->GetStringField(TEXT("sessionHostId"));
+	LogFolder = ConfigJson->GetStringField(TEXT("logFolder"));
+	SharedContentFolder = ConfigJson->GetStringField(TEXT("sharedContentFolder"));
 
-	if (ConfigJson->HasField("certificateFolder"))
+	if (ConfigJson->HasField(TEXT("certificateFolder")))
 	{
-		CertFolder = ConfigJson->GetStringField("certificateFolder");
+		CertFolder = ConfigJson->GetStringField(TEXT("certificateFolder"));
 	}
 	else
 	{
-		CertFolder = "";
+		CertFolder = TEXT("");
 	}
 
-	if (ConfigJson->HasField("gameCertificates"))
+	if (ConfigJson->HasField(TEXT("gameCertificates")))
 	{
-		TSharedPtr<FJsonObject> GameCertsJson = ConfigJson->GetObjectField("gameCertificates");
+		TSharedPtr<FJsonObject> GameCertsJson = ConfigJson->GetObjectField(TEXT("gameCertificates"));
 		for (const auto& GameCert: GameCertsJson->Values)
 		{
 			GameCerts.Add(GameCert.Key, GameCert.Value->AsString());
 		}
 	}
 
-	if (ConfigJson->HasField("buildMetadata"))
+	if (ConfigJson->HasField(TEXT("buildMetadata")))
 	{
-		TSharedPtr<FJsonObject> BuildMetaData = ConfigJson->GetObjectField("buildMetadata");
+		TSharedPtr<FJsonObject> BuildMetaData = ConfigJson->GetObjectField(TEXT("buildMetadata"));
 		for (const auto& BuildMetaDataValue: BuildMetaData->Values)
 		{
 			Metadata.Add(BuildMetaDataValue.Key, BuildMetaDataValue.Value->AsString());
 		}
 	}
 
-	if (ConfigJson->HasField("gamePorts"))
+	if (ConfigJson->HasField(TEXT("gamePorts")))
 	{
-		TSharedPtr<FJsonObject> GamePortsJson = ConfigJson->GetObjectField("gamePorts");
+		TSharedPtr<FJsonObject> GamePortsJson = ConfigJson->GetObjectField(TEXT("gamePorts"));
 		for (const auto& GamePort: GamePortsJson->Values)
 		{
 			Ports.Add(GamePort.Key, GamePort.Value->AsString());
 		}
 	}
 
-	if (ConfigJson->HasField("publicIpV4Address"))
+	if (ConfigJson->HasField(TEXT("publicIpV4Address")))
 	{
-		Ipv4Address = ConfigJson->GetStringField("publicIpV4Address");
+		Ipv4Address = ConfigJson->GetStringField(TEXT("publicIpV4Address"));
 	}
 
-	if (ConfigJson->HasField("fullyQualifiedDomainName"))
+	if (ConfigJson->HasField(TEXT("fullyQualifiedDomainName")))
 	{
-		DomainName = ConfigJson->GetStringField("fullyQualifiedDomainName");
+		DomainName = ConfigJson->GetStringField(TEXT("fullyQualifiedDomainName"));
 	}
 
-	if (ConfigJson->HasField("gameServerConnectionInfo"))
+	if (ConfigJson->HasField(TEXT("gameServerConnectionInfo")))
 	{
-		TSharedPtr<FJsonObject> ConnectionInfoJson = ConfigJson->GetObjectField("gameServerConnectionInfo");
-		TArray<TSharedPtr<FJsonValue>> PortsConfiguration = ConnectionInfoJson->GetArrayField("gamePortsConfiguration");
+		TSharedPtr<FJsonObject> ConnectionInfoJson = ConfigJson->GetObjectField(TEXT("gameServerConnectionInfo"));
+		TArray<TSharedPtr<FJsonValue>> PortsConfiguration = ConnectionInfoJson->GetArrayField(TEXT("gamePortsConfiguration"));
 		TArray<FGamePort> GamePorts;
 
 		for (const auto& PortJson: PortsConfiguration)
 		{
 			auto PortObject = PortJson->AsObject();
 			FGamePort Port;
-			Port.Name = PortObject->GetStringField("name");
-			Port.ServerListeningPort = PortObject->GetNumberField("serverListeningPort");
-			Port.ClientConnectionPort = PortObject->GetNumberField("clientConnectionPort");
+			Port.Name = PortObject->GetStringField(TEXT("name"));
+			Port.ServerListeningPort = PortObject->GetNumberField(TEXT("serverListeningPort"));
+			Port.ClientConnectionPort = PortObject->GetNumberField(TEXT("clientConnectionPort"));
 			GamePorts.Add(Port);
 		}
 
 		FGameServerConnectionInfo GameServerConnectionInfo;
-		GameServerConnectionInfo.PublicIpV4Address = ConnectionInfoJson->GetStringField("publicIpV4Adress");
+		GameServerConnectionInfo.PublicIpV4Address = ConnectionInfoJson->GetStringField(TEXT("publicIpV4Adress"));
 		GameServerConnectionInfo.GamePortsConfiguration = GamePorts;
 		ConnectionInfo = FGameServerConnectionInfo(GameServerConnectionInfo);
 	}
