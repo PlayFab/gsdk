428eec8d356c027d099bd2f6e3ad5047405b03bc
Hayden Wheeler <hello@haydens.tech>
2022-05-04 23:36:10 -0700

    Added -nogsdk flag to allow local ue server testing without MPS agent (#129)
    * Added -nogsdk flag to allow local ue server testing without MPS agent present

--- UnrealPlugin/Source/PlayfabGSDK/Private/PlayfabGSDK.cpp
+++ UnrealPlugin/Source/PlayfabGSDK/Private/PlayfabGSDK.cpp
@@ -35,6 +35,13 @@ DEFINE_LOG_CATEGORY(LogPlayFabGSDK);
 void FPlayFabGSDKModule::StartupModule()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (FParse::Param(FCommandLine::Get(), TEXT("noGSDK")) || FParse::Param(FCommandLine::Get(), TEXT("nogsdk")))
+	{
+		UE_LOG(LogPlayFabGSDK, Display, TEXT("Found noGSDK flag in launch args, no GSDK calls will be made."));
+		NoGSDK = true;
+		return;
+	}
+	
 	GSDKInternal = MakeUnique<FGSDKInternal>();
 
 	GSDKInternal->OnShutdown.BindLambda([this]()
@@ -86,6 +93,7 @@ void FPlayFabGSDKModule::ShutdownModule()
 void FPlayFabGSDKModule::ReadyForPlayers()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return;
 	GSDKInternal->ReadyForPlayers();
 #endif
 }
@@ -93,6 +101,7 @@ void FPlayFabGSDKModule::ReadyForPlayers()
 const FGameServerConnectionInfo FPlayFabGSDKModule::GetGameServerConnectionInfo()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return FGameServerConnectionInfo();
 	return GSDKInternal->GetConnectionInfo();
 #else
 	return FGameServerConnectionInfo();
@@ -102,6 +111,7 @@ const FGameServerConnectionInfo FPlayFabGSDKModule::GetGameServerConnectionInfo(
 FString FPlayFabGSDKModule::GetConfigValue(const FString& Key)
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return TEXT("");
 	return GSDKInternal->GetConfigValue(Key);
 #else
 	return TEXT("");
@@ -111,6 +121,7 @@ FString FPlayFabGSDKModule::GetConfigValue(const FString& Key)
 void FPlayFabGSDKModule::UpdateConnectedPlayers(const TArray<FConnectedPlayer>& CurrentlyConnectedPlayers)
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return;
 	GSDKInternal->SetConnectedPlayers(CurrentlyConnectedPlayers);
 #endif
 }
@@ -118,6 +129,7 @@ void FPlayFabGSDKModule::UpdateConnectedPlayers(const TArray<FConnectedPlayer>& 
 const FString FPlayFabGSDKModule::GetLogsDirectory()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return TEXT("");
 	return GSDKInternal->GetConfigValue(LOG_FOLDER_KEY);
 #endif
 	return TEXT("");
@@ -126,6 +138,7 @@ const FString FPlayFabGSDKModule::GetLogsDirectory()
 const FString FPlayFabGSDKModule::GetSharedContentDirectory()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return TEXT("");
 	return GSDKInternal->GetConfigValue(SHARED_CONTENT_FOLDER_KEY);
 #endif
 
@@ -135,6 +148,7 @@ const FString FPlayFabGSDKModule::GetSharedContentDirectory()
 const TArray<FString> FPlayFabGSDKModule::GetInitialPlayers()
 {
 #if PLAYFAB_GSDK_SERVER
+	if (NoGSDK) return TArray<FString>();
 	return GSDKInternal->GetInitialPlayers();
 #else
 	return TArray<FString>();
