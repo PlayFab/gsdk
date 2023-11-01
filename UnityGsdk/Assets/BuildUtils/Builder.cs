276dc7c3b0f1c01886748037f6f78f4454d4212d
tculotta <tculotta@microsoft.com>
2023-10-31 17:06:53 -0700

    added linux build step

--- UnityGsdk/Assets/BuildUtils/Builder.cs
+++ UnityGsdk/Assets/BuildUtils/Builder.cs
@@ -17,8 +17,8 @@ public class Builder
             },
 
             // Change to location the output should go
-            locationPathName = "BuildOutput/build.exe",
-            options = BuildOptions.CleanBuildCache | BuildOptions.StrictMode,
+            locationPathName = "BuildOutputWindows/build.exe",
+            options = BuildOptions.CleanBuildCache | BuildOptions.BuildScriptsOnly | BuildOptions.StrictMode,
             target = BuildTarget.StandaloneWindows64,
         };
         
@@ -34,10 +34,40 @@ public class Builder
         }
     }
 
+    [MenuItem("Test/Build Linux64")]
+    private static void BuildLinux64()
+    {
+        // Setup build options (e.g. scenes, build output location)
+        var options = new BuildPlayerOptions
+        {
+            // Change to scenes from your project
+            scenes = new[]
+            {
+                "Assets/Scene.unity",
+            },
+
+            // Change to location the output should go
+            locationPathName = "BuildOutputLinux/build.exe",
+            options = BuildOptions.CleanBuildCache | BuildOptions.BuildScriptsOnly | BuildOptions.StrictMode,
+            target = BuildTarget.StandaloneLinux64,
+        };
+
+        var report = BuildPipeline.BuildPlayer(options);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log($"Build successful - Build written to {options.locationPathName}");
+        }
+        else if (report.summary.result == BuildResult.Failed)
+        {
+            Debug.LogError($"Build failed");
+        }
+    }
+
     // This function will be called from the build process
     public static void Build()
     {
-        // Build Win64
         BuildWin64();
+        BuildLinux64();
     }
 }
